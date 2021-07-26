5570ccc804f23f7dda5ab15e4b050667c48ab8c2
tramalho <thiagofacul@gmail.com>
2021-07-25 22:21:04 -0300

    88. Implementing a Countdown Timer Challenge

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var timer: Timer?
+    
+    //minutes to second
+    private let hardness = ["Soft": 5 * 60, "Medium": 7 * 60, "Hard": 12 * 60]
+    
 
-
+    @IBAction func click(_ sender: UIButton) {
+        
+        if let title = sender.currentTitle, let counter = hardness[title]{
+            timer(counter: counter)
+        }
+    }
+    
+    private func timer(counter: Int) {
+        
+        if timer == nil {
+        
+            var period = counter
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { Timer in
+                if period > 0 {
+                    print("\(period) seconds")
+                    period -= 1
+                } else {
+                    Timer.invalidate()
+                    self.timer = nil
+                }
+            }
+        }
+    }
 }
